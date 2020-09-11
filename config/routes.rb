11238dde1db32095f02dd7cb19c1bb70b64c684a
MACOY <valle.hernandez.16199@itsmante.edu.mx>
2020-09-11 17:38:13 -0500

    Primer commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # GET para obtener información
+  # POST enviar información para almacenar de browser -> backend
+  # PUT y PATH orientado al update de los datos
+  # DELETE borar información
+  #resources :users, only [:new,:create]
+
+  
+end
